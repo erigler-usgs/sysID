595f8640abfd61b5baab8cd7a6e2b4d7c5b19b27
jrigler <empty>
2002-06-16 15:05:05 +0000

    The function now takes arbitrary min and max lags, passed via a 2 element vector.  It also returns the autocorrelation regression matrix, and the crosscorrelation matrix if asked.

--- fir_mimo_corls.m
+++ fir_mimo_corls.m
@@ -1,7 +1,7 @@
 ###
 ### fir_mimo_corls.m
 ###
-### This routine will produce a simple Finite Impulse Response (FIR) linear 
+### This routine will produce a coupled Finite Impulse Response (FIR) linear 
 ### filter based on given input and output time series.  This filter is the 
 ### solution to system of linear equations derived from the auto- and cross-
 ### correlation vectors, rather than the actual data-based regression matrix.
@@ -9,30 +9,74 @@
 ### biased estimate of the optimal parameters (that's what was said in 
 ### "Nonlinear System Identification", Nelles (2001) about a nearly identical 
 ### algorithm designed to determine ARX model parameters...since this model 
-### has no recursion, I think it is also guaranteed to be "unbiased").
+### has no recursion, I think it is also guaranteed to be "unbiased", as long
+### as the time series are time-invariant).
 ###
 ### Usage:
 ###
-### [theta] = fir_mimo_corls (its, ots[, lagsize])
+### [theta, lags, lhs, rhs] = fir_mimo_corls (its, ots[, lagsize])
 ###
-### filt     - vector of optimal filter coefficients
-### lag      - vector of corresponding lag values (units are whatever the
-###            sampling interval is)
-### crosscov - full cross-correlation vector for input/output time series
-### autoin   - full auto-correlation vector for the input time series
-### autoout  - full auto-correlation vector for the output time seriess
+### INPUTS:
+###
+### its       - Input time series matrix.  Columns must represent different 
+###             input veriables, rows must represent observations at equally
+###             spaced and monotonically increasing moments in time.
+###
+### ots       - Output time series matrix.  Columns must represent different 
+###             output veriables, rows must represent observations at equally
+###             spaced and monotonically increasing moments in time.
+###
+### lagsize   - Either a scalar, or two element vector.  If a scalar, this
+###             indicates the mimimum and maximum lag about zero lag.  If a
+###             two element vector, lagsize(1) indicates the minimum lag
+###             from zero, and lagsize(2) indicates the maximium lag from zero.
+###
+###
+### OUTPUTS:
+###
+### theta     - vector of optimal filter coefficients
+### lags      - vector of corresponding lag values (units are whatever the
+###             sampling interval is)
+### rhs       - full cross-correlation matrix for output/input time series.
+###             It is the right hand side of the regression equation.
+### lhs       - full auto-correlation matrix for the input time series.
+###             It is the regression matrix, and left hand side of our
+###             equation.  This, multiplied by theta, should give the
+###             rhs matrix.  If not, something is very wrong!
 ###
 
-function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
+function [theta,lag,PHI,CC] = fir_mimo_corls (its, ots, lagsize)
 
   if ( nargin < 2 || nargin > 3)
     usage (["\n\n[filter, lag] = ", \
 	    "fir_mimo_corls (input_ts, output_ts [, lagsize])\n"]);
   endif
 
-  ## Set a default lagsize of 10
+  ## Set a default lagsize of +/-10
   if nargin == 2
     lagsize = 10;
+  endif
+
+  ## If "lagsize" is a two-element vector, then assigne the max value of
+  ## that vector to lagsize, and determine the min and max lag for use
+  ## in calculating the correlation functions
+  if (is_vector(lagsize) && max(size(lagsize)) == 2)
+    min_lag = min(lagsize);
+    max_lag = max(lagsize);
+    maxlagsize = max(abs(lagsize));
+    ## if for some reason a vector was passed of identical values, assume that
+    ## the user wants one negative and one positive
+    if (min_lag == max_lag)
+      min_lag = -min_lag;
+    endif
+  elseif (is_scalar(lagsize) ) ## do nothing
+    min_lag = -lagsize;
+    max_lag = lagsize;
+    maxlagsize = lagsize;
+  else
+    error(["\n\"lagsize\" must be either a scalar, or a two-element vector\n",\
+	   "indicating the min and max lag (relative to zero) for the correlation\n",\
+	   "functions."]);
   endif
 
   ## Make sure the input and output time series matrices are composed of
@@ -45,18 +89,16 @@ function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
     ots = ots';
   endif
 
+
   ## Calculate auto-correlation functions for input series
-  ## (we make it twice the lagsize, so that we can get a square
-  ##  regression matrix when calculating symetric filters about
-  ##  zero lag)
-  autoin = xcorr (its,lagsize*2);
+  autoin = xcorr (its,max_lag-min_lag);
 
   ## Arrange the auto-correlation regression matrix (Left-Hand side)
   dim_phis = [columns(its),columns(its)];
-  PHI = zeros ((2*lagsize+1) * dim_phis(1));
+  PHI = zeros ((max_lag-min_lag+1) * dim_phis(1));
 
-  for i=1:dim_phis(1):(2*lagsize+1)*dim_phis(1)
-    for j=1:dim_phis(2):(2*lagsize+1)*dim_phis(2)
+  for i=1:dim_phis(1):(max_lag-min_lag+1)*dim_phis(1)
+    for j=1:dim_phis(2):(max_lag-min_lag+1)*dim_phis(2)
 
       ## These are indexes for the auto-correlation matrix
       ii = ceil (i/dim_phis(1));
@@ -68,7 +110,7 @@ function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
       ## top of the third and down, etc.).  This is why we reverse the indices, 
       ## then take the transpose here.
       PHI ( i:i+dim_phis(1)-1 , j:j+dim_phis(2)-1 ) = \
-	  reshape (autoin ( abs(jj - ((2*lagsize+1)+1)) + (ii-1),:), \
+	  reshape (autoin ( abs(jj - ((max_lag-min_lag+1)+1)) + (ii-1),:), \
 		   dim_phis(2), dim_phis(1) )';
     endfor
   endfor
@@ -76,23 +118,26 @@ function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
   ## Calculate the cross-correlation functions for the output and input
   ## time series.  This is equivalent to the assignment to "autoin" above,
   ## except that the xcov.m function does not accept a matrix for both
-  ## input and output (X & Y).  Also, according to (Robinson, 1983)
+  ## input and output (X & Y).  Also, according to (Robinson, 1983)...???
   k = 1;
+
+  ## YOU REALLY NEED TO FIGURE OUT WHY YOU DID THIS HERE, AND MAKE SURE ITS
+  ## CORRECT!!!!!!!!
 
   for i=1:columns (ots)
     for j=1:columns (its)
-
-      cross_inout (:,k) = flipud (xcorr (ots (:,i),its(:,j), lagsize) );
+      cross_inout (:,k) = flipud (xcorr (ots (:,i),its(:,j), maxlagsize) );
       k++;
-
     endfor
   endfor
+  cross_inout = cross_inout ( (maxlagsize+1) + min_lag: (maxlagsize+1) + max_lag , : );
+
 
   ## Arrange the cross-correlation matrix (Right-Hand side)
   dim_ccs = [columns(its),columns(ots)];
   CC = reshape (cross_inout (1,:), dim_ccs(2), dim_ccs(1) )';
 
-  for i=2:(2*lagsize+1)
+  for i=2:(max_lag-min_lag+1)
     
     ## If we use the "reshape" command, we must keep in mind that it reshapes
     ## according to FORTRAN standard indexing (meaning that it progresses down
@@ -105,6 +150,7 @@ function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
     ##  auto-correlation matrix!!!)
 
   endfor
+
 
   ## Now use Single Value Decomposition to solve the system of equations
   [U,S,V] = svd (PHI);
@@ -121,7 +167,7 @@ function [theta,lag] = fir_mimo_corls (its, ots, lagsize)
   ## this equation for solving this using the output from SVD, but it is
   ## essentially a form of reverse substitution)
   theta = V*(S \ (U' * CC) );
-  lag = [-lagsize:lagsize];
+  lag = [min_lag:max_lag];
 
   ## ...and that's all folks!
 
